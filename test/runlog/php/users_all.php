c68a3ce9591b21bdf4bdf7fc57ffe4caea7a3b5a
ido shomer <ido74@gmail.com>
2024-01-01 10:30:13 +0200

    test user congif page

--- test/runlog/php/users_all.php
+++ test/runlog/php/users_all.php
@@ -7,6 +7,7 @@
  */
 
 require_once 'ajax_page_init.php';
+require_once 'php/html_page_init.php';
 
 try {
     $conn = getConnection();
@@ -17,8 +18,8 @@ try {
 }
 
 function getUsersRecords($conn) {
-    $sql = "SELECT tl_runners.id as user_id, tl_runners.member_name as member_name,tl_runners.member_num as member_num, tl_runners.email as email, tl_runners.birthday as birthdate, tl_runners.m_show_profile as active_runner FROM  tl_runners order by tl_runners.m_show_profile DESC,tl_runners.member_name";
-    //$sql = "SELECT tl_runners.id as user_id, tl_runners.member_name as member_name,tl_runners.member_num as member_num, tl_runners.email as email, tl_runners.birthday as birthdate, tl_runners.m_show_profile as active_runner FROM tl_runners WHERE tl_runners.id = $memberId";
+    //$sql = "SELECT tl_runners.id as user_id, tl_runners.member_name as member_name,tl_runners.member_num as member_num, tl_runners.email as email, tl_runners.birthday as birthdate, tl_runners.m_show_profile as active_runner FROM  tl_runners order by tl_runners.m_show_profile DESC,tl_runners.member_name";
+    $sql = "SELECT tl_runners.id as user_id, tl_runners.member_name as member_name,tl_runners.member_num as member_num, tl_runners.email as email, tl_runners.birthday as birthdate, tl_runners.m_show_profile as active_runner FROM tl_runners WHERE tl_runners.id = $memberId";
     $stmt = $conn->query($sql);
     $result = $stmt->fetchAll(PDO :: FETCH_ASSOC);
     // loop over the result and get the shoe distance  for each shoe
