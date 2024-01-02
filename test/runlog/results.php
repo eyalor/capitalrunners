d909125509482b7502780b3186bbabd013459c92
ido shomer <ido74@gmail.com>
2024-01-02 18:28:20 +0200

    debug results

--- test/runlog/results.php
+++ test/runlog/results.php
@@ -51,7 +51,6 @@ function drawRunnerTable() {
 
 
 function drawRunnerTableCallback(jsonData) {
-    echo "Start";
     if (jsonData.data == "") {
         if (jsonData.status.ecode == STATUS_ERR) {
             $("#errMsg1").css("color", "red");
