052763d2b5280ba613e522db3f66f87118aa8d8d
ido shomer <ido74@gmail.com>
2024-01-06 17:07:33 +0200

    fix active user

--- test/runlog/users.php
+++ test/runlog/users.php
@@ -113,6 +113,9 @@ function openUserDialog(userName, userMemberNum, userEmail, userId, birthDate, a
 function getUser() {
 	var active = $('#active_runner').is(':checked') ? 1 : 0;
     var sweight = $('#show_weight').is(':checked') ? 1 : 0;
+    if (is_null(sweight)){
+        sweight = 1; 
+    }
 	var user = {
 	    user_name : $('#user_name').val(),
 		member_num : $('#password').val(),
