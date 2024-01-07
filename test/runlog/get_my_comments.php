691a35ce37b1345a972d37b7fedeaa5deac3d61d
ido shomer <ido74@gmail.com>
2024-01-07 10:12:06 +0200

    test

--- test/runlog/get_my_comments.php
+++ test/runlog/get_my_comments.php
@@ -36,7 +36,7 @@ function getDayEventsAsJSON($conn,$date) {
      FROM tl_events events
      JOIN tl_comments comments ON events.id = comments.event_id
      JOIN tl_runners runners ON comments.runner_id = runners.id
-     WHERE events.run_date >= '" . $date . "'  and comments.runner_id IN (select id from tl_runners where m_is_coach=1 or id = '" . $runnerId . "')
+     WHERE events.run_date >= '" . $date . "'  and comments.runner_id IN (select id from tl_runners where m_is_coach=11 or id = '" . $runnerId . "')
      ORDER BY events.id DESC, comments.id ASC";
 
     $stmt = $conn->query($sql);
