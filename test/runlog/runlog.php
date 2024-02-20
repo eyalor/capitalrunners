5b7dd31b66a4aa3dcb02fbc64e1ceebcd990ad1f
ido shomer <ido74@gmail.com>
2024-02-20 18:18:43 +0200

    test 2 notes

--- test/runlog/runlog.php
+++ test/runlog/runlog.php
@@ -204,6 +204,11 @@ require_once 'php/html_page_init.php';
             <div id="notesContainer" style="float:right;"></div>
         </div>
 
+        <div style="margin-top:20px;">
+            <div class="label">פרטים</div>
+            <div id="notesContainer" style="float:right;"></div>
+        </div>
+
     </form>
 </div>
 </body>
