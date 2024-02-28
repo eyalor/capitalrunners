fb47c9d63df126e977ce91c87e2b205654ebccc2
ido shomer <ido74@gmail.com>
2024-02-28 14:41:01 +0200

    testrpe

--- test/runlog/php/get_daily_feed.php
+++ test/runlog/php/get_daily_feed.php
@@ -17,7 +17,7 @@ catch(PDOException $e) {
 function getDayEventsAsJSON($conn, $date) {
     $sql =
 
-"SELECT tl_events.id, tl_runners.member_name AS 'name',tl_runners.member_name AS 'name_a', tl_run_types.type, run_distance, run_time, warmup_distance, cooldown_distance, COALESCE(tl_events.notes, '') AS 'notes', tl_events.run_type_id, pulse, elevation, weight, date_format(tl_events.run_date,'%H:%i') as HourAndMinutes
+"SELECT tl_events.id, tl_runners.member_name AS 'name',tl_runners.rpe_id AS 'rpe', tl_run_types.type, run_distance, run_time, warmup_distance, cooldown_distance, COALESCE(tl_events.notes, '') AS 'notes', tl_events.run_type_id, pulse, elevation, weight, date_format(tl_events.run_date,'%H:%i') as HourAndMinutes
     FROM tl_events
     JOIN tl_runners ON tl_events.runner_id = tl_runners.id
     JOIN tl_run_types ON tl_events.run_type_id = tl_run_types.id
