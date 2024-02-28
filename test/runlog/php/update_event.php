d009be6e3f16f79311c71ed2eabbaed9448e2272
ido shomer <ido74@gmail.com>
2024-02-28 14:01:19 +0200

    test rpe 10

--- test/runlog/php/update_event.php
+++ test/runlog/php/update_event.php
@@ -32,7 +32,7 @@ if(!$validationResult->isValid()) {
 //
 try {
     $conn = getConnection();
-    $sql = "UPDATE  tl_events  SET run_type_id=?,pulse=?,max_pulse=?,elevation=?,weight=?,warmup_time=?,run_time=?,cooldown_time=?,warmup_distance=?,run_distance=?,cooldown_distance=?,notes=?,shoe_id=?,extra_shoe_id=?,course_id=?, rpe_id=? WHERE id=?";
+    $sql = "UPDATE  tl_events  SET run_type_id=?,pulse=?,max_pulse=?,elevation=?,weight=?,warmup_time=?,run_time=?,cooldown_time=?,warmup_distance=?,run_distance=?,cooldown_distance=?,notes=?,shoe_id=?,extra_shoe_id=?,course_id=?, rpe_id=10 WHERE id=?";
     //$sql = "UPDATE tl_events  SET run_type_id=?,warmup_time=? WHERE id=?";
 
     $sth = $conn->prepare($sql);
