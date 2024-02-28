4c1a40b9d08029eb86709d4292e3926c31550326
ido shomer <ido74@gmail.com>
2024-02-28 14:08:11 +0200

    rpe test

--- test/runlog/php/update_event.php
+++ test/runlog/php/update_event.php
@@ -54,7 +54,7 @@ try {
         (int)$eventFields->{"extra_shoe_id"},
         (int)$eventFields->{"course_id"},
         (int)$eventFields->{"event_id"},
-        $eventFields->{"rpe"}
+        (int)$eventFields->{"rpe"}
 
     ));
     if (!$ok) {
