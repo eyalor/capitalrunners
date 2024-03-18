e77a02ce43b596f5a128389b74113ecddf1d5259
ido shomer <ido74@gmail.com>
2024-03-18 21:29:40 +0200

    fix creat euser

--- test/runlog/php/create_user.php
+++ test/runlog/php/create_user.php
@@ -4,7 +4,7 @@
 	$user = getUser();
 try {
 	$conn = getConnection();
-	$sql = 'INSERT INTO tl_runners (member_name,email,member_num,m_show_profile,m_show_weight) VALUES (:user_name,:email,:password,:active_runner)';
+	$sql = 'INSERT INTO tl_runners (member_name,email,member_num,m_show_profile,m_show_weight) VALUES (:user_name,:email,:password,:active_runner,:show_weight)';
 	$sth = $conn->prepare($sql);
 	$ok = $sth->execute(array (
 	':user_name'=> $user->user_name,
