17d10162843406437980bffc9aa9752a8b4cb18f
ido shomer <ido74@gmail.com>
2024-03-19 08:58:24 +0200

    fix create user

--- runlog/php/create_user.php
+++ runlog/php/create_user.php
@@ -4,13 +4,13 @@
 	$user = getUser();
 try {
 	$conn = getConnection();
-	$sql = 'INSERT INTO tl_runners (member_name,email,member_num,m_show_profile,m_show_weight) VALUES (:user_name,:email,:password,:active_runner)';
+	$sql = 'INSERT INTO tl_runners (member_name,email,member_num,m_show_profile,m_show_weight) VALUES (:user_name,:email,:password,:active_runner,:show_weight)';
 	$sth = $conn->prepare($sql);
 	$ok = $sth->execute(array (
 	':user_name'=> $user->user_name,
 	':email' => $user->email,
 	':password' => $user->member_num,
-	':active_runner' => $user->active_runner;
+	':active_runner' => $user->active_runner,
 	':show_weight' => $user->show_weight
 	));
 	if (!$ok) {
