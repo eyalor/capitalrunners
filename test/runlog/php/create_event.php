d43fd7e7b7e4b5d57a46f30da6fd01f88830db31
ido shomer <ido74@gmail.com>
2024-03-19 09:25:37 +0200

    add race id to events

--- test/runlog/php/create_event.php
+++ test/runlog/php/create_event.php
@@ -46,7 +46,7 @@ $the_date = $eventFields-> {"date"};
 // OK - now we have a valid input - lets try to create the event from DB
 try {
     $conn = getConnection();
-    $sql = 'INSERT INTO tl_events (run_date,warmup_time,run_time,cooldown_time,warmup_distance,run_distance,cooldown_distance,notes,runner_id,shoe_id,extra_shoe_id,course_id,run_type_id,pulse,max_pulse,elevation,weight, rpe_id) VALUES (:run_date,:warmup_time,:run_time,:cooldown_time,:warmup_distance,:run_distance,:cooldown_distance,:notes,:runner_id,:shoe_id,:extra_shoe_id,:course_id,:run_type_id,:pulse,:max_pulse,:elevation,:weight,:rpe)';
+    $sql = 'INSERT INTO tl_events (run_date,warmup_time,run_time,cooldown_time,warmup_distance,run_distance,cooldown_distance,notes,runner_id,shoe_id,extra_shoe_id,course_id,run_type_id,pulse,max_pulse,elevation,weight, rpe_id, race_id) VALUES (:run_date,:warmup_time,:run_time,:cooldown_time,:warmup_distance,:run_distance,:cooldown_distance,:notes,:runner_id,:shoe_id,:extra_shoe_id,:course_id,:run_type_id,:pulse,:max_pulse,:elevation,:weight,:rpe,:race_id)';
     $sth = $conn->prepare($sql);
 
     $ok = $sth->execute(array (
@@ -68,6 +68,7 @@ try {
 		':elevation' => $eventFields->{"elevation"},
 		':weight' => $eventFields->{"weight"},
         ':rpe' => $eventFields->{"rpe"},
+        ':race_id' => $eventFields->{"race_id"},
     ));
 
     if (!$ok) {
