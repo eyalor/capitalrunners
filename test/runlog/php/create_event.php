65433925a7935efcee0b00cba5d48383d3cbb5fb
ido shomer <ido74@gmail.com>
2024-03-22 10:45:53 +0200

    test

--- test/runlog/php/create_event.php
+++ test/runlog/php/create_event.php
@@ -71,7 +71,7 @@ try {
         ':race_id' => $eventFields->{"race_id"},
     ));
 
-    if (1==2){
+    if ($eventFields->{"run_type_id"}==8){
         $sql2 = 'INSERT INTO tl_races_data (race_id,runner_id,run_time) VALUES (:race_id,:runner_id,:run_time) ON DUPLICATE KEY UPDATE run_time=:run_time';
         $sth2 = $conn->prepare($sql2);
     
