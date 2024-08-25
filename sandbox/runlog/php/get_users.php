c31fcefbfe908bb88d5a4b67c37b7b6d129b108b
ido shomer <ido74@gmail.com>
2024-08-25 14:50:59 +0300

    get memebr id in get users

--- sandbox/runlog/php/get_users.php
+++ sandbox/runlog/php/get_users.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'ajax_page_init.php';
 
-
+$runnerId = $memberAuthentication->getMemberId();
 try {
 	$conn = getConnection();
 	
@@ -11,7 +11,7 @@ try {
 		$sqlPhrase = "SELECT id AS value ,member_name AS label FROM  tl_runners WHERE member_name like '%" . $search_term  . "%' ORDER BY member_name";
 	} else {
 		$sqlPhrase = "SELECT distinct tl_runners.id,tl_runners.member_name FROM tl_runners WHERE (tl_runners.m_show_profile = '1' ) 
-		order by FIELD(tl_runners.id,18600162,18600094) desc,
+		order by FIELD(tl_runners.id,18600162,'" . $runnerId . "') desc,
 		 tl_runners.member_name"; 
 		//$sqlPhrase = "SELECT distinct tl_runners.id,tl_runners.member_name FROM tl_runners,tl_events WHERE ((run_date > '" . date('Y-m-d', $the_date) . "' and tl_runners.id=tl_events.runner_id) or tl_runners.id = '" . $memberAuthentication->getMemberId() . "') order by tl_runners.member_name"; 
 	}
