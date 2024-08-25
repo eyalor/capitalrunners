74a77a95bf67e4e41fea238dbfa10f2a2135794e
ido shomer <ido74@gmail.com>
2024-08-25 13:20:21 +0300

    add new links in feed

--- sandbox/runlog/feed.php
+++ sandbox/runlog/feed.php
@@ -360,6 +360,10 @@ require_once 'php/html_page_init.php';
     <a id="show_all_events_label" href="#" onclick="showAllComments(); return false;" style="display:none;">הצג את כל האימונים</a>
 	<h2 id="test" class="page_header">      </h2>
 	<a id="show_my_comments_action" href="#" onclick="showMyComments(); return false;"  title="הצג אימונים שלי עם הערות מאמנים בלבד" >הצג הערות מאמנים    </a>
+    <h2 id="test" class="page_header">      </h2>
+	<a id="show_my_comments_action" href="#" onclick="showMyComments(); return false;"  title="הצג אימונים שלי עם הערות מאמנים בלבד" >הצג רק אימונים שלי    </a>
+    <h2 id="test" class="page_header">      </h2>
+	<a id="show_my_comments_action" href="#" onclick="showMyComments(); return false;"  title="הצג אימונים שלי עם הערות מאמנים בלבד" >הצג תוכנית אימונים לשבוע  זה     </a>
 	<div id="comment_menu"></div>
     <div id="feed"></div>
     <div id="show_more">
