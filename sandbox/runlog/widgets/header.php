cd0b3525a38369260e75f3e7b21133b810cf3326
ido shomer <ido74@gmail.com>
2024-09-05 16:38:13 +0300

    change reminder to user

--- sandbox/runlog/widgets/header.php
+++ sandbox/runlog/widgets/header.php
@@ -18,6 +18,7 @@
     $start = (date('w', $ts) == 0) ? $ts : strtotime('last sunday', $ts);
     $start_date = date('Y-m-d', $start);
     $end_date = date('Y-m-d', strtotime('next saturday', $start)); 
+	$runner_id = $memberAuthentication->getMemberId();
 	
 	$link = mysqli_connect("localhost", "u574399506_testlog", "Sandbox1PA$$", "u574399506_testlog") or die("Could not connect");
 
@@ -41,7 +42,7 @@
 	$query_bd = "SELECT member_name AS runner_name FROM  tl_runners WHERE m_show_profile = true and DATE_FORMAT(birthday, '%d-%m') = DATE_FORMAT(CURDATE(), '%d-%m')";
 	$result_bd = mysqli_query($link, $query_bd) or die("Query failed");
 
-	$query_reminder = "SELECT member_name AS runner_name FROM  tl_runners WHERE id in (18600094,18600141)";
+	$query_reminder = "SELECT member_name AS runner_name FROM  tl_runners WHERE tl_runners.id = '" . $runner_id . "'";
 	$result_reminder = mysqli_query($link, $query_reminder) or die("Query failed");
 
 	$admin = $memberAuthentication->isAdmin();
