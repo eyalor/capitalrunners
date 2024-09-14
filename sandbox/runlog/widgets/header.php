b42ab99053865e43547f49db2f10fe0cb0ab0100
ido shomer <ido74@gmail.com>
2024-09-14 20:50:29 +0300

    add runner weekly

--- sandbox/runlog/widgets/header.php
+++ sandbox/runlog/widgets/header.php
@@ -41,6 +41,14 @@
 	
 	$query_bd = "SELECT member_name AS runner_name FROM  tl_runners WHERE m_show_profile = true and DATE_FORMAT(birthday, '%d-%m') = DATE_FORMAT(CURDATE(), '%d-%m')";
 	$result_bd = mysqli_query($link, $query_bd) or die("Query failed");
+
+	$query_r_w = "SELECT (SUM(run_distance) + SUM(warmup_distance) + SUM(cooldown_distance)) as runner_weekly from tl_events where date(run_date) >= '" . $start_date . "' and date(run_date) <= '" . $end_date . "' and  WHERE tl_runners.id = '" . $runner_id . "'";
+	$result_r_w = mysqli_query($link, $query_r_w) or die("Query failed");
+	$row_r_w = mysqli_fetch_array($result_r_w, MYSQLI_ASSOC);
+	
+	if ($row_r_w['runner_weekly'] == "" or $row_r_w['runner_weekly'] == null)
+		$row_r_w['runner_weekly'] = 0;
+
 	$admin = $memberAuthentication->isAdmin();
  	$coach = $memberAuthentication->isCoach();
 	
@@ -55,6 +63,7 @@
       function drawChart() {
 		var k_d = <?php echo $row_d['daily']; ?>;
         var k_w = <?php echo $row_w['weekly']; ?>;
+		var k_r_w = <?php echo $row_w['runner_weekly']; ?>;
         var data_d = google.visualization.arrayToDataTable([
           ['Label', 'Value'],
           ['TODAY', k_d]
@@ -81,8 +90,23 @@
           minorTicks: 10
         };
 
+		var data_r_w = google.visualization.arrayToDataTable([
+          ['Label', 'Value'],
+		  ['WEEK', k_r_w]
+        ]);
+
+        var options_r_w = {
+          width: 200, height: 150,
+          max: 180,
+          redFrom: 150, redTo: 180,
+          yellowFrom: 100, yellowTo: 150,
+          minorTicks: 10
+        };
+
         var chart = new google.visualization.Gauge(document.getElementById('chart_div_d'));
         chart.draw(data_d, options_d);
+		var chart = new google.visualization.Gauge(document.getElementById('chart_div_r_w'));
+		chart.draw(data_r_w, options_r_w);
 		var chart = new google.visualization.Gauge(document.getElementById('chart_div_w'));
 		chart.draw(data_w, options_w);
       }
@@ -94,6 +118,8 @@
 			  <tr >
 			    <td width="30%"><span id="swbdh" style="color:#ffbb99"><b> מזל טוב ליום ההולדת<b></td>
 				<td align="center"><b><font color="white">קילומטרז' כללי היום</font></b></td>
+				<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+				<td align="center"><b><font color="white">קילומטרז' אישי שבועי</font></b></td>
 				<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
 				<td align="center"><b><font color="white">קילומטרז' כללי שבועי</font></b></td>
 				
@@ -115,6 +141,10 @@
 				</td>
 				<td>
 					<div align="center" id='chart_div_d'></div>
+				</td>
+				<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+				<td>
+					<div align="center" id='chart_div_r_w'></div>
 				</td>
 				<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
 				<td>
