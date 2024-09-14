dd0558dd6866a7b0d6eea5d4e67af383a4ebb385
ido shomer <ido74@gmail.com>
2024-09-14 22:18:53 +0300

    hard coded array

--- sandbox/runlog/widgets/header.php
+++ sandbox/runlog/widgets/header.php
@@ -93,7 +93,14 @@
           minorTicks: 10
         };
 
-		var data_r_w = google.visualization.arrayToDataTable([['Label', 'Value'],k_r_w]);
+		var data_r_w = google.visualization.arrayToDataTable([
+			['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+		]);
 
         var options_r_w = {
 			title: 'My Week',
