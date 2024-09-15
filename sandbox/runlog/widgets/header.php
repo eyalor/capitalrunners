4f4adb4fd7c902c0753a08447bd4abf9379532b0
ido shomer <ido74@gmail.com>
2024-09-15 08:59:00 +0300

    final clean before move to prod

--- sandbox/runlog/widgets/header.php
+++ sandbox/runlog/widgets/header.php
@@ -42,10 +42,7 @@
 	$query_bd = "SELECT member_name AS runner_name FROM  tl_runners WHERE m_show_profile = true and DATE_FORMAT(birthday, '%d-%m') = DATE_FORMAT(CURDATE(), '%d-%m')";
 	$result_bd = mysqli_query($link, $query_bd) or die("Query failed");
 
-	//$query_r_w = "SELECT tl_run_types.type as run_type, cast((SUM(run_distance) + SUM(warmup_distance) + SUM(cooldown_distance)) as CHAR)as runner_weekly from tl_events left join tl_run_types on tl_events.run_type_id=tl_run_types.id where date(run_date) >= '" . $start_date . "' and date(run_date) <= '" . $end_date . "' and tl_events.runner_id = '" . $runner_id . "' group by tl_events.run_type_id";
-	// SELECT tl_run_types.type as run_type,(SUM(run_distance) + SUM(warmup_distance) + SUM(cooldown_distance)) as runner_weekly from tl_events left join tl_run_types on tl_events.run_type_id=tl_run_types.id
 	$query_r_w = "SELECT (SUM(run_distance) + SUM(warmup_distance) + SUM(cooldown_distance)) as runner_weekly from tl_events where date(run_date) >= '" . $start_date . "' and date(run_date) <= '" . $end_date . "' and tl_events.runner_id = '" . $runner_id . "'";
-	//$query_r_w = "SELECT (SUM(run_distance) + SUM(warmup_distance) + SUM(cooldown_distance)) as weekly from tl_events where date(run_date) >= '" . $start_date . "' and date(run_date) <= '" . $end_date . "'";
 	$result_r_w = mysqli_query($link, $query_r_w) or die("Query failed");
 	$row_r_w = mysqli_fetch_array($result_r_w, MYSQLI_ASSOC);
 	
