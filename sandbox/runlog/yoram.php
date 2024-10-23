2af70a55be8944c79465e6ab18903b99599618a7
ido shomer <ido74@gmail.com>
2024-10-23 12:50:45 +0300

    remove button

--- sandbox/runlog/yoram.php
+++ sandbox/runlog/yoram.php
@@ -28,30 +28,7 @@ function generateMessage() {
     return "Hello, this message is generated from a PHP function!";
 }
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Call PHP Function on Button Click</title>
-</head>
-<body>
 
-    <!-- Form with a button to trigger the PHP function -->
-    <form method="POST">
-        <button type="submit" name="triggerFunction">Click Me!</button>
-    </form>
-
-    <?php
-    // Check if the form is submitted
-    if (isset($_POST['triggerFunction'])) {
-        // Call the PHP function and display the result
-        echo "<p>" . generateMessage() . "</p>";
-    }
-    ?>
-
-</body>
-</html>
 
 
 try {
